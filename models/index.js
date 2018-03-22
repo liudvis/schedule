7bfda9e3ab56c891734a321fe66af93f9ab40bdc
EC2 Default User <ec2-user@ip-172-31-39-14.us-east-2.compute.internal>
2018-03-22 19:06:50 +0000

    added mongo lab

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/schedule');
+//mongoose.connect('mongodb://localhost/schedule'); // mongodb://liudvis2:miau@ds221609.mlab.com:21609/schedule
+mongoose.connect('mongodb://liudvis2:miau@ds221609.mlab.com:21609/schedule');
 mongoose.Promise = Promise;
 
 module.exports.Schedule = require('./schedule');
