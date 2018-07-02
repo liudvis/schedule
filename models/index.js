e0d44fe0799146a11bdbf1e5cd31672a01bddad1
EC2 Default User <ec2-user@ip-172-31-39-14.us-east-2.compute.internal>
2018-07-02 08:56:30 +0000

    miau

--- models/index.js
+++ models/index.js
@@ -6,4 +6,4 @@ mongoose.Promise = Promise;
 
 module.exports.Schedule = require('./schedule');
 module.exports.User = require('./user');
-module.exports.Ip = require('./ip');
+// module.exports.Ip = require('./ip');
